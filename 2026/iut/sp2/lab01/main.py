2a6c1dcaafc617886dd2e11af71247fcb3cd7a3f
Md. Irfanur Rahman Rafio <irfanurrahman5@iut-dhaka.edu>
2026-04-21 06:02:32 +0600

    Added Introduction and ToC for lab 01

--- 2026/iut/sp2/lab01/main.py
+++ 2026/iut/sp2/lab01/main.py
@@ -8,3 +8,34 @@ class TitleSlide(SectionSlide):
             section_title = r"Title\\Slide",
             **kwargs
         )
+
+class Introduction(BulletSlide):
+    def __init__(self, **kwargs):
+        super().__init__(
+            header_text="Introduction",
+            text_color=BLACK,
+            points = [
+                r"Computers use binary as their fundamental language.",
+                r"All data in a computer (numbers, text, images, audio \& video, programs, even the entire internet) is represented using bits.",
+                r"Although a single bit can only represent two states (ON/OFF), complex information can be represented using sequences of bits.",
+                r"For representing complex information, all users must agree upon some standards.",
+                r"Numbers are represented using a positional number system.",
+                r"Special techniques enable representation of negative numbers.",
+                r"Bitwise operators are operators that operate on individual bits of a number."
+            ],
+            **kwargs
+        )
+
+class TableOfContents(BulletSlide):
+    def __init__(self, **kwargs):
+        super().__init__(
+            header_text="Table of Contents",
+            text_color=BLACK,
+            points = [
+                r"Binary Number System",
+                r"Signed Number Representation",
+                r"Bitwise Operators",
+                r"Applications",
+            ],
+            **kwargs
+        )
